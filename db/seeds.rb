066e3ab0761b64f36965fa884aef9cd54cace8d3
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-04-01 12:01:15 -0400

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ Item.create(
   condition:"used",
   starting_price:5000,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
 Item.create(
   name:"1932 Ford Roadster Model B",
@@ -25,17 +25,17 @@ Item.create(
   condition:"used",
   starting_price:55000,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
-Item.create(
-  name:"Faberge Spider Brooch",
-  image:"http://4.bp.blogspot.com/-awrpTkvFX8M/VJDgUizLiaI/AAAAAAAADqs/Uu3n2fkYxuI/s1600/Fabergex20Spider_7L.jpg",
-  category:"jewelry",
-  condition:"used",
-  starting_price:40000,
-  open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
-)
+# Item.create(
+#   name:"Faberge Spider Brooch",
+#   image:"http://4.bp.blogspot.com/-awrpTkvFX8M/VJDgUizLiaI/AAAAAAAADqs/Uu3n2fkYxuI/s1600/Fabergex20Spider_7L.jpg",
+#   category:"jewelry",
+#   condition:"used",
+#   starting_price:40000,
+#   open: true,
+#   auction_end: DateTime.now()+rand(1..10).minutes
+# )
 Item.create(
   name:"Jimi Hendrix's 1963 Fender Stratocaster Guitar",
   image:"https://static3.srcdn.com/wordpress/wp-content/uploads/2019/03/1963-Fender-Stratocaster-guitar.jpg?q=50&fit=crop&w=740&h=554&dpr=1.5",
@@ -43,7 +43,7 @@ Item.create(
   condition:"used",
   starting_price:100000,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
 Item.create(
   name:"The last violin played on the Titanic",
@@ -52,7 +52,7 @@ Item.create(
   condition:"used",
   starting_price:27500,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
 Item.create(
   name:"Cave Cay (Island, Bahamas)",
@@ -61,7 +61,7 @@ Item.create(
   condition:"new",
   starting_price:10000000,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
 Item.create(
   name:"Batman #1",
@@ -70,7 +70,7 @@ Item.create(
   condition:"used",
   starting_price:80000,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
 Item.create(
   name:"Salvatore Dali Armchair sculpture",
@@ -79,7 +79,7 @@ Item.create(
   condition:"used",
   starting_price:25000,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
 Item.create(
   name:"Louis Vuitton 1920's era Steamer Trunk",
@@ -88,7 +88,7 @@ Item.create(
   condition:"used",
   starting_price:15000,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
 Item.create(
   name:"Apple-1 Computer",
@@ -97,7 +97,7 @@ Item.create(
   condition:"used",
   starting_price:85000,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
 Item.create(
   name:"Tiffany & Co. Antique 18k Pocket Watch",
@@ -106,7 +106,7 @@ Item.create(
   condition:"used",
   starting_price:30000,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
 Item.create(
   name:"Al Capone owned Colt .45 Pistol",
@@ -115,7 +115,7 @@ Item.create(
   condition:"used",
   starting_price:15000,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
 Item.create(
   name:"1986 Nintendo Super Mario Bros",
@@ -124,7 +124,7 @@ Item.create(
   condition:"new",
   starting_price:500000,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
 Item.create(
   name:"Vincent van Gogh original L/'allée des Alyscamps ",
@@ -133,7 +133,34 @@ Item.create(
   condition:"new",
   starting_price:16000000,
   open: true,
-  auction_end: DateTime.now()+rand(1..3).hours
+  auction_end: DateTime.now()+rand(1..10).minutes
+)
+Item.create(
+  name:"Rangyai Island (Thailand)",
+  image:"https://content.phuket101.net/wp-content/uploads/20200618225319/rang-yai-island-1.jpg",
+  category:"real estate",
+  condition:"used",
+  starting_price:95000000,
+  open: true,
+  auction_end: DateTime.now()+rand(1..10).minutes
+)
+Item.create(
+  name:"Mounu Island Resort (Tonga)",
+  image:"https://www.privateislandsonline.com/uploads/resize/_1454_587fc4291df23.jpg-1360-1100.jpg",
+  category:"real estate",
+  condition:"used",
+  starting_price:75000000,
+  open: true,
+  auction_end: DateTime.now()+rand(1..10).minutes
+)
+Item.create(
+  name:"Koro Levu Island (Fiji)",
+  image:"https://www.privateislandsonline.com/uploads/resize/_2173_58c1c43450f73.jpg-1360-1100.jpg",
+  category:"real estate",
+  condition:"used",
+  starting_price:2750000,
+  open: true,
+  auction_end: DateTime.now()+rand(1..10).minutes
 )
 
 Item.all.each do |item|
@@ -142,7 +169,8 @@ Item.all.each do |item|
   Bid.create(
     bid_amount: rand(min..max),
     user_id: rand(1..10),
-    item_id: item.id
+    item_id: item.id,
+    created_at: DateTime.now()-rand(1..10).minutes
   )
 end
 
